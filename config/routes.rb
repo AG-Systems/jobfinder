3b18844c5b3018dd8ed1f6c55f0e2368d02d9e99
Auriga <area511alienware@gmail.com>
2017-01-09 10:02:09 +0000

    Fixed the homepage of showing jobs

--- config/routes.rb
+++ config/routes.rb
@@ -12,4 +12,5 @@ Rails.application.routes.draw do
   get 'new' => 'jobs#new'
   get '/ConnectionCreate', to: 'connection#create'
   get 'connections' => 'connection#index'
+  get 'profile' => 'jobs#profile'
 end
